940d64681bfe8fe9874f900c3bf96ed63c2c8af3
JasonAceJiao <73757817+JasonAceJiao@users.noreply.github.com>
2021-09-02 01:48:00 +0800

    Update Wskey.js

--- Wskey.js
+++ Wskey.js
@@ -1700,8 +1700,6 @@ function GetCookie() {
   if (req.method != 'OPTIONS' && req.headers) {
     const CV = (req.headers['Cookie'] || req.headers['cookie'] || '');
     const ckItems = CV.match(/(pt_key|pt_pin)=.+?;/g);
-    if (/^https:\/\/(me-|)api(\.m|)\.jd\.com\/openUpgrade\/.test(req.url)) {
-      if (ckItems && ckItems.length == 2) {
         const value = CookieUpdate(null, ckItems.join(''))
         if (value.type !== -1) {
           const write = $nobyda.write(JSON.stringify(value.total, null, 2), "CookiesJD")
@@ -1709,22 +1707,6 @@ function GetCookie() {
         } else {
           console.log(`\n用户名: ${value.name}\n与历史京东 [账号${value.item}] Cookie相同, 跳过写入 ⚠️`)
         }
-      } else {
-        throw new Error("写入Cookie失败, 关键值缺失\n可能原因: 非网页获取 ‼️");
-      }
-    } else if (/^https:\/\/ms\.jr\.jd\.com\/gw\/generic\/hy\/h5\/m\/appSign\?/.test(req.url) && req.body) {
-      const value = CookieUpdate(CV, req.body, 'jrBody');
-      if (value.type) {
-        const write = $nobyda.write(JSON.stringify(value.total, null, 2), "CookiesJD")
-        $nobyda.notify(`用户名: ${value.name}`, ``, `获取京东 [账号${value.item}] 钢镚Body${write?`成功 🎉`:`失败 ‼️`}`)
-      } else {
-        throw new Error("写入钢镚Body失败\n未获取该账号Cookie或关键值缺失‼️");
-      }
-    } else if (req.url === 'http://www.apple.com/') {
-      throw new Error("类型错误, 手动运行请选择上下文环境为Cron ⚠️");
-    }
-  } else if (!req.headers) {
-    throw new Error("写入Cookie失败, 请检查匹配URL或配置内脚本类型 ⚠️");
   }
 }
 
